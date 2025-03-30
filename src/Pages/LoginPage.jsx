4b826d47c28dcb265e4859bf1f7f7848f7dfcb1e
apple <apple@apples-MacBook-Pro.local>
2025-03-30 16:52:07 +0530

    login UI

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Register from '../Components/Register'
+import Login from '../Components/Login'
 
 const LoginPage = () => {
+
+  const [isLogin, setIsLogin] = useState(true)
+
   return (
-    <div>
-      
+    <div className='login-page'>
+      <div className='login-card w-1/3 mx-auto mt-20 bg-white shadow-lg rounded-lg p-8'>
+        <h2 className='text-2xl font-bold text-center mb-6'>Login</h2>
+
+        {/* Register */}
+        
+
+
+        {/* Login */}
+        
+    {isLogin ? (
+      <div>
+        <Login />
+          <p>Don't have an account?</p>
+          <a onClick={() =>  setIsLogin(false)} className='text-blue-500 hover:underline cursor:pointer'>Register here</a>
+        </div>
+    ) : (
+      <div>
+        <Register />
+        <p>Already have an account?</p>
+        <a onClick={() => setIsLogin(true)} className='text-blue-500 hover:underline'>Login here</a>
+      </div>
+    )}
+        
+
+
+      </div>
     </div>
   )
 }
