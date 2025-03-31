e68bcc3f8373dcefbb294d98c044ae2d4eca98f4
apple <apple@apples-MacBook-Pro.local>
2025-03-31 14:14:49 +0530

    project added for each user

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authSlice from './authSlice';
+import projectReducer from "./projectSlice";
+
 
 const store = configureStore({ 
     reducer: {
         auth: authSlice,
+        projects: projectReducer,
     }
 
 })
